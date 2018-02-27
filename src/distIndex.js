55c413c7ef8517aa0bf980e0b146565665d80b27
Johnny Magrippis <j@magrippis.com>
2018-02-27 19:33:19 +0000

    Actually export Login button

--- src/distIndex.js
+++ src/distIndex.js
@@ -1,6 +1,7 @@
 import Provider from './components/Provider'
 
 export { default as withGoogleyolo } from './components/withGoogleyolo'
+export { default as Login } from './components/Login'
 export { default as Logout } from './components/Logout'
 export { default as GoogleyoloShape } from './GoogleyoloShape'
 
