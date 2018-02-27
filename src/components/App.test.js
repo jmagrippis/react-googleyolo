866f13b612a48ecfd24420fa20cfa04057e17f07
Johnny Magrippis <j@magrippis.com>
2018-02-27 19:35:35 +0000

    Sets up enzyme for testing

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { mount } from 'enzyme'
+
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const component = mount(<App />)
+  expect(component).toBeDefined()
+})
